32969ec603757d8b68cb77c4e5ff97e0324240e0
Nupoor <nupoor.roy@tekacademy.com>
2015-12-14 18:11:55 +0530

    project commited

--- nodeauth/app.js
+++ nodeauth/app.js
@@ -64,7 +64,7 @@ app.use(expressValidator({
 
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+//connect-flash
 app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
